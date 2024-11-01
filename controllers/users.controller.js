e04d2e06e9df4e363985d8656e7f641e41f8c74f
Hgauna <hgauna@iprodha.misiones.gov.ar>
2024-11-01 12:48:34 -0300

    starting to return structure requested in requirements

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,11 +1,25 @@
 const db = require("../database/models/index.js");
 const servicesDB = require("../services/services_db.js");
 
+const url = process.env.URL;
+
 const usersController = {
+	//Tiene que devolver un objeto literal con la cantidad de usuarios
 	getAll: async (req, res) => {
 		try {
 			const users = await servicesDB.getAll();
-			res.json(users);
+			res.json({
+				count: users.length,
+				users: users.map((user) => {
+					return {
+						id: user.id,
+						name: user.name + " " + user.lastname,
+						email: user.email,
+						avatar: `${url}/images/avatars/${user.image}`,
+						detail: `${url}/api/users/${user.id}`,
+					};
+				}),
+			});
 		} catch (error) {
 			console.log("Error al buscar los usuarios", error.message);
 		}
@@ -21,11 +35,34 @@ const usersController = {
     getOneById: async (req, res) => {
         try {
             const user = await db.Customer.findByPk(req.params.id);
-            res.json(user);
+            res.json({
+				user: {
+					id: user.id,
+					name: user.name + " " + user.lastname,
+					email: user.email,
+					phone: user.phone,
+					adress: user.adress,
+					country: user.country,
+					state: user.state,
+					//revisar la imagen
+					avatar: `${url}/images/avatars/${user.image}`,
+					detail: `${url}/api/users/${user.id}`
+				}
+			});
         } catch (error) {
             console.log("Error al buscar el usuario", error.message);
         }
-    }
+    },
+	count: async (req, res) => {
+		try {
+			const users = await servicesDB.getAll();
+			res.json({
+				count: users.length,
+			});
+		} catch (error) {
+			console.log("Error al buscar los usuarios", error.message);
+		}			
+	},
 };
 
 module.exports = usersController;
--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
 
+router.get('/count', usersController.count)
 router.get('/getall', usersController.getAll);
 router.get('/getone/:email', usersController.getOneByEmail);
 router.get('/getone/:id', usersController.getOneById);
