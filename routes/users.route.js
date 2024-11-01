0c5d4a3e153035b370feff08d5604c84bd80cb61
hGauna <undrbug@gmail.com>
2024-11-01 18:36:21 -0300

    Modifying the path to display the user image

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,8 +4,8 @@ const usersController = require('../controllers/users.controller');
 
 router.get('/count', usersController.count)
 router.get('/', usersController.getAll);
-router.get('/getonebyemail/:email', usersController.getOneByEmail);
 router.get('/:id', usersController.getbyid);
+router.get('/getbyemail/:email', usersController.getOneByEmail);
 
 
 module.exports = router;
